fe616844958f3800a3f62969f97a0a2de9401143
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-04-11 17:07:21 +0530

    Update email subject for session notifications to include class recording availability

--- src/lib/notifications/email/email.notification.service.ts
+++ src/lib/notifications/email/email.notification.service.ts
@@ -80,7 +80,7 @@ async function sendNotifySessionEmails(
         await sendEmail({
           from: process.env.EMAIL_SENDER!,
           to: task.to,
-          subject: `Upcoming Class Notification`,
+          subject: `Your ${task.class_name} Class Recording Is Now Available`,
           html: html,
           text: text,
         });
