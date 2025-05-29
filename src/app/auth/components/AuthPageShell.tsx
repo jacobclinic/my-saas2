12a9c4f5a19971e793258e4ce00432e97ec6636f
Prabath <prabath0061@gmail.com>
2025-05-29 11:52:22 +0530

    auth shell responsive update

--- src/app/auth/components/AuthPageShell.tsx
+++ src/app/auth/components/AuthPageShell.tsx
@@ -4,7 +4,7 @@ function AuthPageShell({ children }: React.PropsWithChildren) {
   return (
     <div
       className={
-        'flex h-screen flex-col items-center justify-center space-y-4' +
+        'flex min-h-screen flex-col items-center justify-center space-y-4 p-12' +
         ' md:space-y-8 xl:space-y-16' +
         ' animate-in fade-in slide-in-from-top-8 duration-1000' + ' bg-gradient-to-br from-primary-800 via-primary-800 to-secondary-600 opacity-90'
       }
